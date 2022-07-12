d595be1ae6be8e320e5cff87d0c2a49d4d310676
Tom Gregor <tgregor@ivsoftware.com>
2022-07-12 17:58:45 -0600

    Add project files.

---
+++ fill_board_8x8/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace fill_board_8x8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.AutoSize = true;
+            this.tableLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel.ColumnCount = 8;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel.Location = new System.Drawing.Point(37, 57);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 8;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(650, 649);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 788);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+    }
+}
+
---
+++ fill_board_8x8/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace fill_board_8x8
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // Board is a TableLayoutPanel 8 x 8
+            tableLayoutPanel.AutoSize = true;
+            tableLayoutPanel.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            // Add squares
+            IterateBoard(addSquare);
+            // Add pieces
+            IterateBoard(initImage);
+        }
+
+        void IterateBoard(Action<int, int> action)
+        {
+            for (int column = 0; column < 8; column++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    action(column, row);
+                }
+            }
+        }
+
+        private void addSquare(int column, int row)
+        {
+            var color = ((column + row) % 2) == 0 ? Color.White : Color.Black;
+            var square = new Square(column, row, color);
+            tableLayoutPanel.Controls.Add(square, column, row);
+            // Hook the mouse events here
+            square.Click += onSquareClicked;
+            square.MouseHover += onSquareMouseHover;
+        }
+
+        private void onSquareClicked(object sender, EventArgs e) =>
+            MessageBox.Show($"Clicked: {sender}");
+
+        private void onSquareMouseHover(object sender, EventArgs e) =>
+            _tt.SetToolTip((Control)sender, sender.ToString());
+
+        ToolTip _tt = new ToolTip();
+
+        private void initImage(int column, int row)
+        {
+            var square = (Square)tableLayoutPanel.GetControlFromPosition(column, row);
+            switch (row)
+            {
+                case 0:
+                case 1:
+                    square.Piece = Piece.Black;
+                    break;
+                case 6:
+                case 7:
+                    square.Piece = Piece.Red;
+                    break;
+                default:
+                    square.Piece = Piece.None;
+                    break;
+            }
+        }
+    }
+    enum Piece { None, Black, Red };
+    class Square : PictureBox // Gives more visual control than Button
+    {
+        static ToolTip _tt = new ToolTip();
+        public Square(int column, int row, Color color)
+        {
+            BackColor = color;
+            Column = column;
+            Row = row;
+            Size = new Size(80, 80);
+            Margin = new Padding(0);
+            Padding = new Padding(10);
+            Anchor = (AnchorStyles)0xf;
+            SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        public int Column { get; }
+        public int Row { get; }
+        Piece _piece = Piece.None;
+        public Piece Piece
+        {
+            get => _piece;
+            set
+            {
+                if(!Equals(_piece, value))
+                {
+                    _piece = value;
+                    switch (_piece)
+                    {
+                        case Piece.None:
+                            Image = null;
+                            break;
+                        case Piece.Black:
+                            Image = Resources.black;
+                            break;
+                        case Piece.Red:
+                            Image = Resources.red;
+                            break;
+                    }
+                }
+            }
+        }
+        public override string ToString() =>
+            Piece == Piece.None ? 
+                $"Empty {BackColor.Name} square [column:{Column} row:{Row}]" :
+                $"{Piece} piece [column:{Column} row:{Row}]";
+    }
+}
