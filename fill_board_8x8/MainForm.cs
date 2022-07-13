920a5e3eb453fe8c344d1a493e39cc692025c3ce
Tom Gregor <tgregor@ivsoftware.com>
2022-07-12 19:32:41 -0600

    Prelim working version.

--- fill_board_8x8/MainForm.cs
+++ fill_board_8x8/MainForm.cs
@@ -18,35 +18,25 @@ namespace fill_board_8x8
             IterateBoard(initImage);
         }
 
-        void IterateBoard(Action<int, int> action)
-        {
-            for (int column = 0; column < 8; column++)
-            {
-                for (int row = 0; row < 8; row++)
-                {
-                    action(column, row);
-                }
-            }
-        }
-
         private void addSquare(int column, int row)
         {
             var color = ((column + row) % 2) == 0 ? Color.White : Color.Black;
-            var square = new Square(column, row, color);
+            var square = new Square
+            {
+                BackColor = color,
+                Column = column,
+                Row = row,
+                Size = new Size(80, 80),
+                Margin = new Padding(0),
+                Padding = new Padding(10),
+                Anchor = (AnchorStyles)0xf,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+            };
             tableLayoutPanel.Controls.Add(square, column, row);
             // Hook the mouse events here
             square.Click += onSquareClicked;
             square.MouseHover += onSquareMouseHover;
         }
-
-        private void onSquareClicked(object sender, EventArgs e) =>
-            MessageBox.Show($"Clicked: {sender}");
-
-        private void onSquareMouseHover(object sender, EventArgs e) =>
-            _tt.SetToolTip((Control)sender, sender.ToString());
-
-        ToolTip _tt = new ToolTip();
-
         private void initImage(int column, int row)
         {
             var square = (Square)tableLayoutPanel.GetControlFromPosition(column, row);
@@ -65,24 +55,30 @@ namespace fill_board_8x8
                     break;
             }
         }
+
+        void IterateBoard(Action<int, int> action)
+        {
+            for (int column = 0; column < 8; column++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    action(column, row);
+                }
+            }
+        }
+
+        private void onSquareClicked(object sender, EventArgs e) =>
+            MessageBox.Show($"Clicked: {sender}");
+
+        private void onSquareMouseHover(object sender, EventArgs e) =>
+            _tt.SetToolTip((Control)sender, sender.ToString());
+
+        ToolTip _tt = new ToolTip();
     }
-    enum Piece { None, Black, Red };
     class Square : PictureBox // Gives more visual control than Button
     {
-        static ToolTip _tt = new ToolTip();
-        public Square(int column, int row, Color color)
-        {
-            BackColor = color;
-            Column = column;
-            Row = row;
-            Size = new Size(80, 80);
-            Margin = new Padding(0);
-            Padding = new Padding(10);
-            Anchor = (AnchorStyles)0xf;
-            SizeMode = PictureBoxSizeMode.StretchImage;
-        }
-        public int Column { get; }
-        public int Row { get; }
+        public int Column { get; internal set; }
+        public int Row { get; internal set; }
         Piece _piece = Piece.None;
         public Piece Piece
         {
@@ -112,4 +108,5 @@ namespace fill_board_8x8
                 $"Empty {BackColor.Name} square [column:{Column} row:{Row}]" :
                 $"{Piece} piece [column:{Column} row:{Row}]";
     }
+    enum Piece { None, Black, Red };
 }
